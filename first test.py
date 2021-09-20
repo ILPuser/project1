13127090081944c56a7736c4cbf776ac9d4e898c
varunsamvedi@gmail.com <varunsamvedi@gmail.com>
2021-09-20 15:24:44 +0530

    20th sep 15:24

--- first test.py
+++ first test.py
@@ -1,1 +1,16 @@
-print('text')
+
+#Example 6: Create a nonlocal variable
+#Example 6: Create a nonlocal variable
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    inner()
+    print("outer:", x)
+
+
+outer()
