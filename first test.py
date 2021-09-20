8f53810a631bc66567959c0061d61862498fdd73
varunsamvedi@gmail.com <varunsamvedi@gmail.com>
2021-09-20 19:30:49 +0530

    decorators closures etc...

--- first test.py
+++ first test.py
@@ -1,11 +1,14 @@
+# NONLOCAL and GLOBAL
 
 #Example 6: Create a nonlocal variable
-#Example 6: Create a nonlocal variable
+#nonlocal allows us to assign values to an outer (but non-global) scope.
+#for assigning values to the global scope, we can use global keyword instead.
 def outer():
     x = "local"
 
     def inner():
-        nonlocal x
+        #nonlocal x
+        
         x = "nonlocal"
         print("inner:", x)
 
@@ -14,3 +17,33 @@ def outer():
 
 
 outer()
+########################################
+
+
+# YIELD in Python
+
+# Yield is a keyword in Python that is used to return from a function without destroying the states of its local variable and # when the function is called, the execution starts from the last yield statement. Any function that contains a yield keyword # is termed a generator. Hence, yield is what makes a generator.27-May-2021
+
+
+# Decorators in Python
+
+#-------- orig_func already given ----------
+def orig_func():
+   print("Wheee!")
+
+#------ write decorator ------------
+def my_decorator(some_function):
+    def my_wrapper():
+        print ("do something before")   #do something before, if you want to
+        some_function()
+        print ("do something after")    #do something after, if you want to
+    return my_wrapper
+
+#------ create decorator and call orig func --------
+orig_func = my_decorator(orig_func)   #create decorator, modify functioning 
+orig_func()  
+
+
+# Closures in Python
+ #the inner_func has the full access to the environment (variables) in it’s enclosing scope. That is closure.
+ 
